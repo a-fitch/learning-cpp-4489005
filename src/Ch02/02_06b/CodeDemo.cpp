c8a37a5ee5cb8d86b4121e4174706476dd9f696a
a-fitch <ANGELA_MARTINEZ820@YAHOO.COM>
2025-05-21 23:42:06 +0000

    added a comment

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -1,10 +1,22 @@
 // Learning C++ 
-// Exercise 02_06
+// Exercise 02_06 Completed
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstdint>
+
+#define CAPACITY 5000
+// #define DEBUG to add comment to result, otherwise hidden
 
 int main(){
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+#ifdef DEBUG
+    std::cout << "[About to perform the addition]" << std::endl;
+#endif
+    large += small; // or "large = large + small;"
+    std::cout << "The large integer is " << large << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
