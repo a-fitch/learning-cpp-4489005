80fd1bf2673d6d09d025ad68ae2ee489e17f547d
a-fitch <ANGELA_MARTINEZ820@YAHOO.COM>
2025-05-21 20:32:59 +0000

    add a comment

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -1,12 +1,30 @@
 // Learning C++ 
-// Exercise 02_03
+// Exercise 02_03 Completed
 // Using Variables, by Eduardo Corpeño 
 
 #include <iostream>
 
+int a, b = 5; // line comment
+
+/* multi
+* line
+* comment */
+
 int main(){
-    std::cout << "Hi There!" << std::endl;
-    
+    bool my_flag;
+    a = 7;
+    my_flag = false;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "flag = " << my_flag << std::endl;
+    my_flag = true;
+    std::cout << "flag = " << my_flag << std::endl;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "b - a = " << b - a << std::endl;
+    unsigned int positive; // unsigned example resulted in binary
+    positive = b - a;
+    std::cout << "b - a (unsigned) = " << positive << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
