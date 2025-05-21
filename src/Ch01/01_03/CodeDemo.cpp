a13863ae3f9d09f086c29ff5202d21a3c08c97e8
a-fitch <ANGELA_MARTINEZ820@YAHOO.COM>
2025-05-21 19:22:11 +0000

    added a comment

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Learning C++ 
-// Challenge 01_03
+// Challenge 01_03 Completed
 // Console Interaction, by Eduardo Corpeño 
 
 #include <iostream>
@@ -7,8 +7,9 @@
 
 int main(){
     std::string str;
+    std::cout << "Please enter your name: " << std::flush;
     std::cin >> str;
-    std::cout << str;
+    std::cout << "Hello, " << str << "!" << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
